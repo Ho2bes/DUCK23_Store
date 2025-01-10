e27bdc78fc76db0d0cc8c3ab89a3e446cbb18121
Ho2bes <8696@holbertonstudents.com>
2025-01-10 10:48:01 +0100

    change setting.py for the secret and modification of yml for dependencies

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -1,6 +1,17 @@
 module.exports = function (config) {
   config.set({
-    browsers: ['ChromeHeadless'],
-    // Ajoutez d'autres configurations ici
+    browsers: ['ChromeHeadless'], // Utilisez ChromeHeadless au lieu de Chrome
+    singleRun: true, // Assurez-vous que les tests s'arrêtent après une exécution
+    frameworks: ['jasmine'],
+    files: [
+      'src/**/*.spec.ts',
+    ],
+    preprocessors: {
+      'src/**/*.spec.ts': ['webpack'],
+    },
+    reporters: ['progress'],
+    webpack: {
+      // Votre configuration Webpack ici
+    },
   });
 };
