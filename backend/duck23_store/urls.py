81f8f44b111ce1a3d109284aa5c9a05c18081fa8
Ho2bes <8696@holbertonstudents.com>
2025-02-13 09:13:45 +0100

    change the path of the token

--- backend/duck23_store/urls.py
+++ backend/duck23_store/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from .views import home_view, test_view # Import de la vue d'accueil
 
 urlpatterns = [
@@ -28,6 +28,7 @@ urlpatterns = [
     # URLs pour l'authentification avec JWT
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
     # Page d'accueil
     path('', home_view, name='home'),
