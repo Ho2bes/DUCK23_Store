ac2a2724d3cfdb4f125dc571c3c55d3529aab5a3
Ho2bes <8696@holbertonstudents.com>
2025-02-18 16:10:36 +0100

    modif admin

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+# Enregistre le modèle User dans Django Admin
+admin.site.register(User, UserAdmin)
