f1ca314a5d7671fe8a63a31b41640c71b02f9ffc
Ho2bes <8696@holbertonstudents.com>
2025-03-06 01:19:16 +0100

    update tests back to debug

--- backend/duck23_store/test.py
+++ backend/duck23_store/test.py
@@ -15,15 +15,3 @@ class ServerTests(TestCase):
             client = APIClient()
             response = client.get('/api/nonexistent-route/')
             self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-
-        def test_server_error_handling(self):
-            client = Client()
-        # Simuler une erreur interne en appelant une vue qui lève une exception
-            response = client.get('/api/trigger-internal-error/')
-            self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-        def test_health_check(self):
-            client = Client()
-            response = client.get('/api/healthcheck/')
-            self.assertEqual(response.status_code, status.HTTP_200_OK)
-            self.assertEqual(response.data, {"status": "ok"})
